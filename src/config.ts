ba6d769788e378e91969c6003b35c14e47b3917e
ranjan <ranjansingh3357@gmail.com>
2026-05-05 10:00:00 +0530

    chore: setup environment and base config

---
+++ src/config.ts
@@ -0,0 +1,24 @@
+export const FONT_FAMILY_OPTIONS = [
+  'Arial',
+  'Courier New',
+  'Georgia',
+  'Times New Roman',
+  'Verdana'
+];
+
+export const FONT_SIZE_OPTIONS = [
+  '12px',
+  '15px',
+  '18px',
+  '24px',
+  '32px'
+];
+
+export const BLOCK_TYPE_TO_LABEL = {
+  paragraph: 'Normal',
+  h1: 'Heading 1',
+  h2: 'Heading 2',
+  h3: 'Heading 3',
+  bullet: 'Bullet List',
+  number: 'Numbered List',
+};
